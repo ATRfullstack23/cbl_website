94a2ad15ce9755b6fea1015a1bfd86cfd176a20b
Amal B Nair <amal004nair@gmail.com>
2024-07-30 15:18:10 +0530

    Changes in custom table ui

--- src/lib/dashboard_utils/CustomTable.svelte
+++ src/lib/dashboard_utils/CustomTable.svelte
@@ -14,69 +14,89 @@
     }
 </script>
 
-<div class="w_full_sales_data" style="min-width:{item.width}; height:{item.height};">
-    <table>
-        <thead>
-            <tr>
-                {#each item.table_column_data as table_column}
-                    <th class="table_column_name"
-                        >{table_column.display_name}</th
-                    >
-                {/each}
-            </tr>
-        </thead>
-        <tbody>
-            {#each report_data as table_data}
-                <tr class="full_row">
+<div class="w_full_sales_data" style="min-width:{item.width}px;">
+    <div class="contents_container">
+        {#if item.title}
+            <h5>{item.title}</h5>
+        {/if}
+        {#if item.description}
+            <p>{item.description}</p>
+        {/if}
+    </div>
+    <div class="outer_table_container" style="width: 100%; max-height: {item.height}px; overflow: auto;">
+        <table>
+            <thead>
+                <tr>
                     {#each item.table_column_data as table_column}
-                        <td>{table_data[table_column.id_value_column]}</td>
+                        <th class="table_column_name"
+                            >{table_column.display_name}</th
+                        >
                     {/each}
                 </tr>
-            {/each}
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                {#each report_data as table_data}
+                    <tr class="full_row">
+                        {#each item.table_column_data as table_column}
+                            <td data-column_id = '{table_data[table_column.id_value_column]}'>{table_data[table_column.id_value_column]}</td>
+                        {/each}
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <style>
     table {
         width: 100%;
-        border_collapse: collapse;
-        margin: 20px 0;
+        border-collapse: collapse;
+        border: 1px solid #e3e3e3;
+        background-color: #f9fafb;
+        /* margin: 20px 0; */
     }
-    th,
-    td {
-        border_bottom: 1px solid #ddd;
+    th,td {
+        border-bottom: 1px solid #ddd;
         padding: 13px;
-        text_align: left;
-        border_top: 1px solid #ddd;
+        text-align: left;
+        border-top: 1px solid #ddd;
     }
     th {
-        background_color: #f2f2f2;
+        background-color: #f2f2f2;
         color: black;
     }
 
     tr:hover {
-        background_color: #f1f1f1;
+        background-color: #f1f1f1;
     }
     .full_row {
         color: rgb(0, 0, 0);
     }
-    .full_row:last_child {
+    /* .full_row:last-child {
         color: black;
-        font_size: 20px;
-        font_weight: 500;
-    }
+        font-size: 20px;
+        font-weight: 500;
+    } */
     .content {
-        text_align: left;
+        text-align: left;
     }
-    .table_column_name:first_child {
-        text_align: left;
+    .table_column_name:first-child {
+        text-align: left;
     }
     .table_column_name {
-        text_align: right;
+        /* text-align: right; */
     }
-    /* .w_full_sales_data {
-        width: 90%;
-        margin: 0 auto;
-    } */
+    .w_full_sales_data {
+        background-color:#fff;
+        padding: 10px;
+    }
+    .contents_container h5{
+        font-size: 22px;
+        font-weight: 500;
+        padding-top: 10px;
+    }
+    .contents_container p{
+        font-size: 16px;
+        color: #6a6868;
+    }
 </style>
