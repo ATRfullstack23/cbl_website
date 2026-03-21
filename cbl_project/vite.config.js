b69f3b58b1da2fa5ad7213564bd75d018bade35c
Amaldev <tramaldev@gmail.com>
2026-03-21 19:56:49 +0530

    Fix Vite env config and base path issue

--- cbl_project/vite.config.js
+++ cbl_project/vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-  export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || '/cbl_website',
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || '/cbl_website',
+  })
+}
