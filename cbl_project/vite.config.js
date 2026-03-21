1d2d04b70430a596f332cbb57f399efcd6fd1840
Amaldev T R <116741508+ATRfullstack23@users.noreply.github.com>
2026-03-21 19:00:13 +0530

    Update vite.config.js

--- cbl_project/vite.config.js
+++ cbl_project/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+  export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: import.meta.env.VITE_BASE_URL || '/cbl_website/',
 });
